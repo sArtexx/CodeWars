bd467b1a4f94716e24b0f3ebe8267fe004a61a08
Simonenko Ignat <simonenko_ik@ukr.net>
2017-10-26 13:13:31 +0300

    com

---
+++ src/MinMax.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Write a function that returns both the minimum and maximum number of the given list/array.
+ */
+
+public class MinMax {
+    public static int[] minMax(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(Arrays.stream(arr).min().getAsInt());
+        list.add(Arrays.stream(arr).max().getAsInt());
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
